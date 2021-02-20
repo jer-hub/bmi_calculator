dfb02ce3bcfce1c3cba0aead7f79f1dedaf0f0ae
jer-hub <jerwin.carreon@jmc.edu.ph>
2021-02-20 17:25:24 +0800

    updated

--- bmi/views.py
+++ bmi/views.py
@@ -3,15 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-
-    height = float(request.GET.get('height',1))
-    weight = float(request.GET.get('weight',2))
-
-    bmi = float(weight/(height**2))
-
-    return render(request, 'bmi/home.html', {
-        'thebmi' : bmi
-    })
+    return render(request, 'bmi/home.html')
 
 def result(request):
-    return render(request, 'bmi/result.html')
+    height = float(request.GET.get('height',1))
+    weight = float(request.GET.get('weight',1))
+
+    bmi = float(weight/(height**2))
+    return render(request, 'bmi/result.html', {
+        'thebmi' : bmi
+    })
